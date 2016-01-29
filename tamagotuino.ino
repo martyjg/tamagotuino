dfbaa87b42f7178197d1d6934b195b4fddffc389
martyjg <martinjosephgormley@gmail.com>
2016-01-29 18:44:35 +0000

    Added button on pin 6 to break loop and feed pet when first stage of hunger is reached

--- tamagotuino.ino
+++ tamagotuino.ino
@@ -1,28 +1,58 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
+const int feedButton = 6;
 int fullness = 10;
+bool isHungry = false;
+
+int activatedFeedButton = 0;
 
 void setup() {
   lcd.begin(16, 2);
-  lcd.print("Pet is content");
-  lcd.setCursor(0, 1);
-  lcd.print("(^_^)");
+  pinMode(feedButton, INPUT);
+  
+
 }
 
 void loop() {
+
+  activatedFeedButton = digitalRead(feedButton);
+//      if (activatedFeedButton == HIGH) {
+//      fullness = 10;
+//    }
+
+  
   if (fullness <= 0) {
     diedDisplay();
-  } else if (fullness < 5) {
+  } else if (fullness < 6) {
     gettingHungryDisplay();
+    isHungry = true;
+    live();
+  } else {
+    contentDisplay();
+    live();
   }
-live();
 
+    if (activatedFeedButton == HIGH && isHungry == true) {
+      isHungry = false;
+      fullness = 10;
+    }
 }
 
 void live() {
-  delay(500);
+  delay(800);
   fullness--;
+}
+
+void feed() {
+  fullness = 10;
+}
+
+void contentDisplay() {
+  lcd.clear();
+  lcd.print("Pet is content");
+  lcd.setCursor(0, 1);
+  lcd.print("(^_^)");
 }
 
 void gettingHungryDisplay() {

1fb72af829bb972c6bf9ddda111ec3bcf0abe808
martyjg <martinjosephgormley@gmail.com>
2016-01-30 17:19:31 +0000

    Added a check function that changes display when temperature falls too low

--- tamagotuino.ino
+++ tamagotuino.ino
@@ -3,22 +3,34 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 const int feedButton = 6;
 const int heatSensor = A0;
-const float baselineTemp = 19.0;
+const float baselineTemp = 22.0;
 int fullness = 10;
-bool isHungry = false;
+float rawTemperature;
+int temperature;
+bool isHungry;
+bool isCold;
 
 int activatedFeedButton = 0;
 
 void setup() {
+  Serial.begin(9600);
   lcd.begin(16, 2);
   pinMode(feedButton, INPUT);
-  
+
+  checkTemperature();
+
+  contentDisplay();
 
 }
 
 void loop() {
 
   activatedFeedButton = digitalRead(feedButton);
+  int heatValue = analogRead(heatSensor);
+  Serial.print("Sensor Value: ");
+  Serial.print(heatValue);
+
+  checkTemperature();
   
   if (fullness <= 0) {
     diedDisplay();
@@ -26,6 +38,8 @@ void loop() {
     gettingHungryDisplay();
     isHungry = true;
     live();
+  } else if (isCold == true) {
+    gettingColdDisplay();
   } else {
     contentDisplay();
     live();
@@ -33,18 +47,34 @@ void loop() {
 
   if (activatedFeedButton == HIGH && isHungry == true) {
     isHungry = false;
-    fullness = 10;
+    fullness = 30;
   }
   
 }
 
 void live() {
-  delay(1000);
+  delay(500);
   fullness--;
 }
 
 void feed() {
   fullness = 10;
+}
+
+void checkTemperature() {
+  int heatValue = analogRead(heatSensor);
+  float voltage = (heatValue/1024.0) * 5;
+  rawTemperature = (voltage -.5) * 100;
+  Serial.print(", Volts: ");
+  Serial.print(voltage);
+  temperature = (int) rawTemperature;
+  
+  if (temperature < 24) {
+    isCold = true;
+  } else {
+    isCold = false;
+  }
+  
 }
 
 void contentDisplay() {
@@ -60,6 +90,15 @@ void gettingHungryDisplay() {
   lcd.print("Getting hungry");
   lcd.setCursor(0, 1);
   lcd.print("(o_o)");
+}
+
+void gettingColdDisplay() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Getting cold");
+  lcd.setCursor(0, 1);
+  lcd.print("(~_~)");
+  delay(100);
 }
 
 void diedDisplay() {
